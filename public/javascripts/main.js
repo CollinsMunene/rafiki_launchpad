5135a1f497dfcbe4101d92fa057a8ffac2325e01
Collins Hillary <hillarycollins@protonmail.com>
2024-12-19 13:59:29 +0300

    feat:update baseURL

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://rafiki-launchpad.devligence.com";
 
 const getOrCreateToken = async () => {
   let token = localStorage.getItem("rafikiLaunchPadToken");
--- public/javascripts/new_instance.js
+++ public/javascripts/new_instance.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://rafiki-launchpad.devligence.com";
 
 document.getElementById("instanceName").addEventListener("input", function (event) {
     const input = event.target;
